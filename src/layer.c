09e3e26cee2c38a8ce046dbfb84c058f917c75d6
Julian Squires <julian@cipht.net>
2014-06-07 15:47:12 -0400

    Clean up layer code a little
    Hopefully as this proceeds, some of these pieces can become more generic.

--- src/layer.c
+++ src/layer.c
@@ -47,33 +47,39 @@ static void request_another_chunk(struct layer *t)
     t->last = succ(i);
 }
 
-static void draw_bottom_up(struct layer *t, float top, float bottom)
+static void ensure_ith_chunk(struct layer *t, unsigned i)
 {
-    float p = t->position;
+    if (i == t->last)
+        request_another_chunk(t);
+    ENSURE(t->ring[i]);
+}
+
+static void find_the_lowest_visible_chunk(struct layer *t, float bottom)
+{
     unsigned i = t->first;
-
-    // get to the bottom of the screen
     do {
-        if (i == t->last)
-            request_another_chunk(t);
-        ENSURE(t->ring[i]);
-        if (p-t->ring[i]->map.h < bottom) break;    /* p is now visible */
-        p -= t->ring[i]->map.h;  /* move p up one chunk */
+        ensure_ith_chunk(t, i);
+        if (t->position-t->ring[i]->map.h < bottom) break;
         t->position -= t->ring[i]->map.h;
-        i = succ(i);               /* next chunk */
-    } while(true);
-    t->first = i;  /* this is the new first chunk */
-
-    // draw while there's visible space left
-    do {
-        if (i == t->last)
-            request_another_chunk(t);
-        ENSURE(t->ring[i]);
-        if (p <= top) break;
-        p -= t->ring[i]->map.h;
-        tilemap_draw(t->ring[i], I*p);
         i = succ(i);
     } while(true);
+    t->first = i;
+}
+
+static void draw_up_while_space_available(struct layer *t, float top)
+{
+    unsigned i = t->first;
+    for(float p = t->position; p > top; i = succ(i)) {
+        ensure_ith_chunk(t, i);
+        p -= t->ring[i]->map.h;
+        tilemap_draw(t->ring[i], I*p);
+    }
+}
+
+static void draw_bottom_up(struct layer *t, float top, float bottom)
+{
+    find_the_lowest_visible_chunk(t, bottom);
+    draw_up_while_space_available(t, top);
 }
 
 void layer_draw(struct layer *t)
@@ -92,38 +98,3 @@ void layer_draw(struct layer *t)
         ENSURE(false);
     }
 }
-
-/* 
- * void camera_scroll(float amount_px)
- * {
- *     world_camera.translation += elapsed_time * scroll_speed;
- *     camera.bottom += amount_px;
- * }
- */
-
-
-/* 
- * void map_advance(void)
- * {
- *     int i, j;
- * 
- *     float camera_top = world_camera.translation,
- *        camera_bottom = viewport_h + camera_top;
- * 
- *     for (i = 0; chunks[i].t && chunks[i].p + chunks[i].t->map.h < camera_top);
- * 
- *     // XXX memcpy
- *     for (j = 0; chunks[i].t; ++j, ++i) chunks[j] = chunks[i];
- * 
- *     for (; 0 == j || chunks[j-1].bottom < camera_top; ++j) {
- *         ENSURE(j < n_chunks);
- *         chunks[j] = chunks[0];
- *         chunks[j].bottom = chunks[j-1].bottom + chunks[j-1].t->map.h;
- *         chunks[j].top = chunks[j].bottom + chunks[j].height;
- *         if (j == 0) signal_next_chunk(); /\* XXX wait, is that right? *\/
- *     }
- *     chunks[j] = NULL;
- *     for (i = 0; chunks[i]; ++i)
- *         render(chunks[i], chunks[i].bottom - camera.y);
- * }
- */
